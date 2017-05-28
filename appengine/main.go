eb178bd5deece9137ac2bf17fee0679945f2f76c
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2017-05-27 19:20:19 -0700

    Verify deep linking

--- appengine/main.go
+++ appengine/main.go
@@ -33,6 +33,15 @@ const (
 	stopErrMsg     = `You did not run /start command yet.`
 
 	msgTemplate = "From %s:\n%s"
+
+	verifyContent = `[{
+  "relation": ["delegate_permission/common.handle_all_urls"],
+  "target": {
+    "namespace": "android_app",
+    "package_name": "com.yhsif.notifbot",
+    "sha256_cert_fingerprints": ["FB:36:90:89:B8:AA:02:69:EF:D6:6D:D6:C7:48:7E:D8:1A:26:6D:A5:07:18:62:04:3D:C7:70:8D:75:6A:2C:22"]
+  }
+}]`
 )
 
 var urlRegexp = regexp.MustCompile(clientPrefix + `(\d+)/(.+)`)
@@ -43,6 +52,7 @@ func main() {
 	http.HandleFunc(clientPrefix, clientHandler)
 	http.HandleFunc("/_ah/health", healthCheckHandler)
 	http.HandleFunc("/_ah/start", initHandler)
+	http.HandleFunc("/.well-known/assetlinks.json", verifyHandler)
 	appengine.Main()
 }
 
@@ -57,6 +67,11 @@ func initHandler(w http.ResponseWriter, r *http.Request) {
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "healthy")
+}
+
+func verifyHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	fmt.Fprint(w, verifyContent)
 }
 
 func webhookHandler(w http.ResponseWriter, r *http.Request) {
