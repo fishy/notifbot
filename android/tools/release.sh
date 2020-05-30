7c1b43a01e5f18e030a4b9131f24fe7e2ce78518
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2020-05-30 10:54:06 -0700

    android: Release build improvement

--- android/tools/release.sh
+++ android/tools/release.sh
@@ -5,7 +5,7 @@ set -e
 keyfile=${1:-"${HOME}/.android/release.jks"}
 tempfile=`mktemp -u app.tmp.apk.XXXXXXXXXX`
 
-bazel build :app -c opt
+bazel build --config=release :app
 zipalign -p 4 bazel-bin/app_unsigned.apk ${tempfile}
 apksigner sign --ks ${keyfile} --out app.apk ${tempfile}
 rm ${tempfile}
