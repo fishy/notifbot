a1ebf4f8fb6ba6edf35950aa86781f36dd706cbb
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2021-12-25 02:01:03 -0800

    android: Update release.sh

--- android/tools/release.sh
+++ android/tools/release.sh
@@ -3,9 +3,9 @@
 set -e
 
 keyfile=${1:-"${HOME}/.android/release.jks"}
-tempfile=`mktemp -u app.tmp.apk.XXXXXXXXXX`
+tempfile=$(mktemp -u app.tmp.apk.XXXXXXXXXX)
 
 bazel build --config=release :app
-zipalign -p 4 bazel-bin/app_unsigned.apk ${tempfile}
-apksigner sign --ks ${keyfile} --out app.apk ${tempfile}
-rm ${tempfile}
+zipalign -p 4 bazel-bin/app_unsigned.apk "${tempfile}"
+apksigner sign --ks "${keyfile}" --out app.apk "${tempfile}"
+rm "${tempfile}"
