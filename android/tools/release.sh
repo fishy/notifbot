e5d7ea6222a02b1728ecaa7ed57be67994c767bd
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2022-05-22 08:56:01 -0700

    android: Switch to aab

--- android/tools/release.sh
+++ android/tools/release.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-keyfile=${1:-"${HOME}/.android/release.jks"}
-tempfile=$(mktemp -u app.tmp.apk.XXXXXXXXXX)
+keyfile=${1:-"${HOME}/.android/upload.jks"}
 
-bazel build --config=release :app
-zipalign -p 4 bazel-bin/app_unsigned.apk "${tempfile}"
-apksigner sign --ks "${keyfile}" --out app.apk "${tempfile}"
-rm "${tempfile}"
+target="release"
+output="app.aab"
+tempfile=$(mktemp -u app.tmp.aab.XXXXXXXXXX)
+
+bazel build --config=release ":${target}"
+cp "bazel-bin/${target}.aab" "${tempfile}"
+jarsigner -keystore "${keyfile}" "${tempfile}" upload
+mv "${tempfile}" "${output}"
