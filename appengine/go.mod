e8d450515d482491e84a822b631bff25e4187100
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2022-08-23 19:45:56 -0700

    appengine: 404 everything
    Force everyone to upgrade the app and switch to cloudrun backend.

--- appengine/go.mod
+++ appengine/go.mod
@@ -3,12 +3,9 @@ module github.com/fishy/notifbot/appengine
 go 1.16
 
 require (
-	cloud.google.com/go/datastore v1.6.0
-	cloud.google.com/go/monitoring v1.1.0
-	cloud.google.com/go/secretmanager v1.0.0
-	github.com/blendle/zapdriver v1.3.1
-	go.uber.org/zap v1.19.1
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/appengine/v2 v2.0.1
-	google.golang.org/genproto v0.0.0-20211018162055-cf77aa76bad2
-	google.golang.org/protobuf v1.27.1
+	google.golang.org/protobuf v1.27.1 // indirect
 )
--- appengine/main.go
+++ appengine/main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
-	"regexp"
-	"strconv"
-	"strings"
 
-	"cloud.google.com/go/datastore"
 	"google.golang.org/appengine/v2"
 )
 
 const (
-	globalURLPrefix = `https://notifbot.fishy.me`
-	clientPrefix    = `/c/`
-
-	fieldLabel = "label"
-	fieldMsg   = "msg"
-
-	playURL = `https://play.google.com/store/apps/details?id=com.yhsif.notifbot`
-
-	msgTemplate = "From %s:\n%s"
-
 	verifyContent = `[{
   "relation": ["delegate_permission/common.handle_all_urls"],
   "target": {
@@ -33,35 +18,12 @@ const (
 }]`
 )
 
-var urlRegexp = regexp.MustCompile(clientPrefix + `(\-?\d+)/(.+)`)
-
-var dsClient *datastore.Client
-
 func main() {
-	initLogger()
-
-	ctx := context.Background()
-	if err := initDatastoreClient(ctx); err != nil {
-		l(ctx).Fatalw(
-			"Failed to get data store client",
-			"err", err,
-		)
-	}
-	initBot(ctx)
-
 	http.HandleFunc("/", rootHandler)
-	http.HandleFunc(clientPrefix, clientHandler)
 	http.HandleFunc("/_ah/health", healthCheckHandler)
 	http.HandleFunc("/.well-known/assetlinks.json", verifyHandler)
-	go chatCounterMetricsLoop()
 
 	appengine.Main()
-}
-
-func initDatastoreClient(ctx context.Context) error {
-	var err error
-	dsClient, err = datastore.NewClient(ctx, getProjectID())
-	return err
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
@@ -71,54 +33,6 @@ func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 func verifyHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	fmt.Fprint(w, verifyContent)
-}
-
-func clientHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := logContext(r)
-
-	groups := urlRegexp.FindStringSubmatch(r.URL.Path)
-	if len(groups) == 0 {
-		http.NotFound(w, r)
-		return
-	}
-	id, err := strconv.ParseInt(groups[1], 10, 64)
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
-	ctx = logContextWith(ctx, "chat-id", id)
-
-	chatCounter.Inc(id)
-
-	chat := GetChat(ctx, id)
-	if chat == nil || chat.Token != groups[2] {
-		http.NotFound(w, r)
-		return
-	}
-
-	if r.Method != http.MethodPost {
-		w.Header().Set("Location", playURL)
-		w.WriteHeader(http.StatusSeeOther)
-		return
-	}
-
-	label := r.FormValue(fieldLabel)
-	msg := r.FormValue(fieldMsg)
-	if label == "" || msg == "" {
-		http.NotFound(w, r)
-		return
-	}
-	if !strings.HasPrefix(strings.TrimSpace(msg), strings.TrimSpace(label)) {
-		// For some apps, the notification title will just be app name, so only add
-		// label when that's not the case to avoid repetition.
-		msg = fmt.Sprintf(msgTemplate, label, msg)
-	}
-	if getToken().SendMessage(ctx, id, msg) == http.StatusUnauthorized {
-		l(ctx).Info("Invalidating secret cache and retrying...")
-		initBot(ctx)
-		getToken().SendMessage(ctx, id, msg)
-	}
-	w.WriteHeader(http.StatusOK)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
